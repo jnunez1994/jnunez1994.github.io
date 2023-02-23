4a96518337a2c3a6ec38e45bc66b4873e91b654f
Jorge Nunez <jorgenunez3021@gmail.com>
2023-02-23 14:52:08 -0300

    Cambios en reto Google

--- Reto-ToDoList/script.js
+++ Reto-ToDoList/script.js
@@ -1,19 +1,22 @@
-let nombreT = document.getElementById("recipient-name").value
-let tareaT= document.getElementById("tipos").value
-let descripcionT= document.getElementById('message-text').value
+let nombreT = document.getElementById("recipient-name")
+let tareaT= document.getElementById("tipos")
+let descripcionT= document.getElementById('message-text')
 
 let listaT= document.getElementById('tareasTodas')
 
-let nuevaTarea = `<li class="list-group-item d-flex justify-content-between align-items-start">
-                    <div class="ms-2 me-auto">
-                        <div class="fw-bold">${nombreT} </div>
-                        ${descripcionT}
-                    </div>
-                    <span class="badge bg-primary rounded-pill">14</span>
-                </li> `
+
 
 let addButton = document.getElementById('addData').addEventListener('click', function(){
-        listaT.innerHTML += nuevaTarea
+    let nuevaTarea = `<li class="list-group-item d-flex justify-content-between align-items-start">
+    <div class="ms-2 me-auto">
+        <div class="fw-bold">${nombreT.value} </div>
+        ${descripcionT.value}
+    </div>
+    <span class="badge bg-primary rounded-pill">14</span>
+</li> `
+    
+    
+    listaT.innerHTML += nuevaTarea
     
 })
 
