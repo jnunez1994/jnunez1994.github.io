fe8a0a385570fea45d0b5113709cc7507a10d78d
Jorge Nunez <jorgenunez3021@gmail.com>
2023-03-01 18:33:59 -0300

    Reto 03 con cambios, sin terminar

--- Reto-ToDoList/script.js
+++ Reto-ToDoList/script.js
@@ -1,22 +1,45 @@
-//datos del modal
+let masBtn = document.querySelector('.favButton')
+let cerrarBtn = document.querySelector('.cerrarPop')
+
+let popup = document.querySelector('.popup')
+
+//datos del form
 let nombreT = document.getElementById("nombreTarea");
 let tipoT = document.getElementById("tipoTarea");
 let descripcionT = document.getElementById("descripcionTarea");
-//let prioridadT = ;
 
-let listaT = document.getElementById("contenedorLista");
+
+let listaT = document.getElementById('contenedorLista')
+
+
+
+
+//btn + para abrir popup
+masBtn.addEventListener('click', () => {
+    popup.classList.add('active')
+    masBtn.disabled=true
+})
+
+//btn X para cerrar popup
+cerrarBtn.addEventListener('click',() => {
+    popup.classList.remove('active')
+    masBtn.disabled=false
+})
+
+
+
+
+
+
 
 //Botón agregar y función
-let add = document.getElementById("addData");
+let add = document.getElementById('addData')
+add.addEventListener('click', () => {
 
-add.addEventListener("click", () => {
-
-    let nuevaT = `<div class="lista">
-            <h1>${nombreT.value} (${tipoT.value})</h1>
-            
-            <p>${descripcionT.value}</p>
-                <div>`
+    let nuevaT = `
+            <h1>${nombreT.value} (${tipoT.value})</h1>`
 
                 listaT.innerHTML += nuevaT;
 
 })
+
