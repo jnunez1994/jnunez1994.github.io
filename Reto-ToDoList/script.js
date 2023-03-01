0c97baa7a1fd895a07a03ed4a0c6695bb3c96b63
Jorge Nunez <jorgenunez3021@gmail.com>
2023-03-01 20:26:25 -0300

    Reto 3

--- Reto-ToDoList/script.js
+++ Reto-ToDoList/script.js
@@ -1,15 +1,23 @@
+
 let masBtn = document.querySelector('.favButton')
 let cerrarBtn = document.querySelector('.cerrarPop')
+let cerrarBtn2 = document.getElementById('cerrar')
+
+let inicio = document.querySelector('.Inicio')
 
 let popup = document.querySelector('.popup')
 
+//contenedor de todas las tareas
+let listaT = document.getElementById("tareasT")
+
 //datos del form
+let formTareas = document.querySelector('#datosTareas')
+
 let nombreT = document.getElementById("nombreTarea");
 let tipoT = document.getElementById("tipoTarea");
 let descripcionT = document.getElementById("descripcionTarea");
 
 
-let listaT = document.getElementById('contenedorLista')
 
 
 
@@ -26,6 +34,12 @@ cerrarBtn.addEventListener('click',() => {
     masBtn.disabled=false
 })
 
+//btn cerrar para cerrar popup
+cerrarBtn2.addEventListener('click',() => {
+    popup.classList.remove('active')
+    masBtn.disabled=false
+})
+
 
 
 
@@ -33,13 +47,17 @@ cerrarBtn.addEventListener('click',() => {
 
 
 //Botón agregar y función
-let add = document.getElementById('addData')
-add.addEventListener('click', () => {
 
-    let nuevaT = `
-            <h1>${nombreT.value} (${tipoT.value})</h1>`
+let addButton = document.getElementById('addData').addEventListener('click', function(){
+        
+    let nuevaT = `<div class="lista">
+                <h5>${nombreT.value}</h5>
+                </div>`
+                
+        listaT.innerHTML += nuevaT
 
-                listaT.innerHTML += nuevaT;
-
+        popup.classList.remove('active')
+      
+        masBtn.disabled=false
 })
 
